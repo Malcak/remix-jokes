ab6e13433448803db0cd437d5c6f5fe9278f39f0
Malcak <malcakx@gmail.com>
2022-01-25 00:18:15 -0500

    feat(style): add styling to main views

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,37 @@
+import type { LinksFunction } from 'remix'
+
 import {
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration,
+  ScrollRestoration
 } from 'remix'
 import type { MetaFunction } from 'remix'
+
+import globalStylesUrl from './styles/global.css'
+import globalMediumStylesUrl from './styles/global-medium.css'
+import globalLargeStylesUrl from './styles/global-large.css'
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: 'stylesheet',
+      href: globalStylesUrl
+    },
+    {
+      rel: 'stylesheet',
+      href: globalMediumStylesUrl,
+      media: 'print, (min-width: 640px)'
+    },
+    {
+      rel: 'stylesheet',
+      href: globalLargeStylesUrl,
+      media: 'screen and (min-width: 1024px)'
+    }
+  ]
+}
 
 export const meta: MetaFunction = () => {
   return { title: 'New Remix App' }
@@ -14,10 +39,10 @@ export const meta: MetaFunction = () => {
 
 export default function App() {
   return (
-    <html lang='en'>
+    <html lang="en">
       <head>
-        <meta charSet='utf-8' />
-        <meta name='viewport' content='width=device-width,initial-scale=1' />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
       </head>
