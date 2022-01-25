5b67f0faf5d7df32b169af3b30b92c11bf07963e
Malcak <malcakx@gmail.com>
2022-01-24 22:49:15 -0500

    feat(jokes): add default jokes route

---
+++ app/routes/jokes/index.tsx
@@ -0,0 +1,8 @@
+export default function JokesIndexRoute() {
+  return (
+    <div>
+      <p>Here&apos;s a random joke:</p>
+      <p>I was wondering why the frisbee was getting bigger, the it hit me</p>
+    </div>
+  )
+}
